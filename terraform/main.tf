e367dd947fa1b3d11e2d986ed7e349bd3868edfa
Biswajit Dash <biswajit.azuredevops@gmail.com>
2025-12-16 20:40:17 +0530

    ansible and terraform

---
+++ terraform/main.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+      version = "~> 2.16"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "web1" {
+  name  = "web1"
+  image = docker_image.nginx.latest
+  ports {
+    internal = 80
+    external = 8081
+  }
+}
+
+resource "docker_container" "web2" {
+  name  = "web2"
+  image = docker_image.nginx.latest
+  ports {
+    internal = 80
+    external = 8082
+  }
+}
+
+output "web1_ip" {
+  value = docker_container.web1.ip_address
+}
+
+output "web2_ip" {
+  value = docker_container.web2.ip_address
+}
